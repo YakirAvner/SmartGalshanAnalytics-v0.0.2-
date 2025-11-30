3737309327288d72e0a97d62b3c1efa99c43f8ee
yakir <yakir.dukchaiev@gmail.com>
2025-11-30 17:52:56 +0200

    ReadME status explains most of the work

--- SG-ExcelAnalytics/ip_db_copier.py
+++ SG-ExcelAnalytics/ip_db_copier.py
@@ -1,13 +1,8 @@
-import pandas as pd
-import csv
 import os
-import glob
-import openpyxl
-from pathlib import Path
 import paramiko as pmk
+import posixpath
 import socket
-
-BASE_DIR = Path(__file__).resolve().parent
+import stat
 
 
 class IP_DB_Copier:
@@ -15,13 +10,24 @@ class IP_DB_Copier:
         self.device_name_list = device_name_list
         self.device_ip_list = device_ip_list
 
-    def copy_db(self):
-        local_dir = r"C:\\Users\\user\Desktop\\Yakir Avner\\SG_Devices"
-        if not os.path.exists(local_dir):
-            os.mkdir(local_dir)
-            print("This folder doesn't exist!!!")
+    def copy_folder(self, sftp, local_dir, phone_source_folder):
+        os.makedirs(local_dir, exist_ok=True)
+        for files_or_folders in sftp.listdir_attr(phone_source_folder):
+            local_item = os.path.join(local_dir, files_or_folders.filename)
+            remote_item = posixpath.join(
+                phone_source_folder, files_or_folders.filename)
+            if stat.S_ISDIR(files_or_folders.st_mode):
+                self.copy_folder(sftp, local_item, remote_item)
+            else:
+                if not os.path.exists(local_item):
+                    sftp.get(remote_item, local_item)
+                    print(f"Copied {remote_item} to {local_item}")
+                else:
+                    print(
+                        f"The {local_item} is already copied in the local directory")
 
     def connect_to_SGPhone(self):
+        base_local_dir = r"C:\\Users\\user\Desktop\\Yakir Avner\\SG_Devices"
         for phone_name, ip in zip(self.device_name_list, self.device_ip_list):
             # Connecting to each DB in the list.
             # Define the database connector from the given IP
@@ -31,9 +37,8 @@ class IP_DB_Copier:
             except ValueError:
                 print(f"Invalid IP format: {ip}")
                 continue
-            port = int(port)
-            username = input("Enter Username: ")
-            password = input("Enter Password: ")
+            username = "g188"
+            password = "1470"
             phone_source_folder = r"/Documents"
 
             client = pmk.SSHClient()
@@ -43,9 +48,8 @@ class IP_DB_Copier:
                 client.connect(hostname=hostname, port=port,
                                username=username, password=password)
                 sftp = client.open_sftp()
-                remote_dir = sftp.listdir(phone_source_folder)
-                for db_date in phone_source_folder:
-                    print(db_date)
+                local_dir = os.path.join(base_local_dir, phone_name)
+                self.copy_folder(sftp, local_dir, phone_source_folder)
 
             except (pmk.SSHException, socket.timeout, TimeoutError, OSError) as e:
                 print(
